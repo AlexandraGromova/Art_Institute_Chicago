f79d6c6489d56ecd070a6353a89ea88e1cd56f6d
Sasha Gromova <sasadanilina2@gmail.com>
2023-08-02 00:45:26 +0600

    Initial Commit

---
+++ ArtInstituteChicago/ArtInstituteChicagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtInstituteChicagoApp.swift
+//  ArtInstituteChicago
+//
+//  Created by Sasha on 02.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ArtInstituteChicagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
